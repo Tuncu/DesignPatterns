8ced98cb7f2fd86765805a74e6ae28f1e158519a
Tunc Ulusoy <ulusoytunc@hotmail.com>
2017-12-20 13:27:56 +0300

    DesignPatterns

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoogerFactory());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+    public class LoogerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //İş geliştirme
+            return new LogNetLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+    public class TuLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with TuLooger");
+        }
+    }
+
+    public class LogNetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with LogNetLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        ILoggerFactory _loggerFactory;
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager=CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+    }
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {         
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        //test
+        public void Save()
+        {
+            Console.WriteLine("Kaydedildi");
+        }
+    }
+
+    //multi-thread de kimi zaman aynı anda üretilebiliyor
+    class CustomerManager1
+    {
+        private static CustomerManager1 _customerManager1;
+        static object _lockObject = new object();
+        private CustomerManager1()
+        {
+
+        }
+
+        public static CustomerManager1 CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager1==null)
+                    _customerManager1 = new CustomerManager1();      
+            }
+            return _customerManager1;
+        }
+
+        //test
+        public void Save()
+        {
+            Console.WriteLine("Kaydedildi");
+        }
+    }
+}
